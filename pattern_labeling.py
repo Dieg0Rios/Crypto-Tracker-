299a032cbbf441012da010b6cfc8bbb24ffd6969
Dieg0Rios <diego.rios@upr.edu>
2025-05-29 00:08:30 -0400

    Added the Bullish Harami Pattern

--- pattern_labeling.py
+++ pattern_labeling.py
@@ -1,7 +1,14 @@
-from candle_info import getopen, gethigh, getlow, getclose 
+from candle_info import Candle
 import pandas as pd
 
 df = pd.read_csv("D:\Crypto\Crypto-Tracker-\Crypto\data\solana_history.csv")
 
 def isbullishengulfing(candle1, candle2):
-    if 
+    if candle1.getopen() > candle1.getclose() and candle2.getopen() < candle2.getclose():
+        if candle1.getclose() > candle2.getopen() and candle1.getopen() < candle2.getclose():
+            return True
+    return False
+
+def isbullishharami(candle1,candle2): 
+    if candle1.getopen() >candle1.getclose() and ca
+
